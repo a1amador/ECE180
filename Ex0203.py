ee480ca4b7a1fb26935eaf92079123bb47d58d11
a1amador <36138286+a1amador@users.noreply.github.com>
2018-02-04 16:16:44 -0800

    Update Ex0203.py

--- Ex0203.py
+++ Ex0203.py
@@ -12,16 +12,6 @@
 # search free but our one
 # other do no information time
 
-# the of and to a
-# in for is on that
-# by this with i you
-# it not or be are
-# from at as your all
-# have new more an was
-# we will home can us
-# about if page my has
-# search free but our one
-# other do no information time
 
 # If the last group has less than five at the end, just write out the last group.
 
@@ -32,29 +22,27 @@ u = 'https://raw.githubusercontent.com/first20hours/google-10000-english/master/
 response = urllib2.urlopen(u)
 words = [i.strip() for i in response.readlines()]
 
-fname = 'output_by_5.txt'
-f = open(fname, 'w')  # write mode
-words_5=[]
-w5 = []
-cc=0
-for i in words:
-    cc = cc+1
-    if cc%5==0:
-        f.write(i + '\n')
-    else:
-        f.write(i + ' ')
-
-f.close()
 
 
-
-
-fname = 'output_by_5.txt'
 def write_chunks_of_five(words,fname):
     '''
+    Given a list of words, create a new file that consists of each consecutive non-overlapping
+    sequence of five lines merged into one line.
     :param: words
     :type: list
     :param: fname
     :type: str
     '''
-# YOUR CODE HERE
+    f = open(fname, 'w')  # write mode
+    cc = 0
+    for i in words:
+        cc = cc + 1
+        if cc % 5 == 0:
+            f.write(i + '\n')
+        else:
+            f.write(i + ' ')
+
+    f.close()
+
+fname = 'output_by_5.txt'
+write_chunks_of_five(words,fname)
